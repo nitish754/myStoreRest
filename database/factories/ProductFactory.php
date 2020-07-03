f97fda63fa8207a5c7510eb20d181c811100b3c1
nitish754 <ernitish1993@gmail.com>
2020-07-03 02:38:21 -0700

    database seeding done

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        "product_name" => $faker->word,
+        "product_details" => $faker->paragraph,
+        "price" => $faker->numberBetween(1000,10000),
+        'discount' => $faker->numberBetween(100,500),
+        "stock" => $faker->randomDigit,
+
     ];
 });
