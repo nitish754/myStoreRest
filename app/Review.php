1f7e0c737e15906c96d8d486878a53dc9ef1a990
nitish754 <ernitish1993@gmail.com>
2020-07-03 03:17:21 -0700

    Created Eloquoent Relation

--- app/Review.php
+++ app/Review.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Product;
 
 class Review extends Model
 {
-    //
+    public function Products()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
