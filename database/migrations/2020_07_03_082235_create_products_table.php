09d2c33ef668d8b7a701d0e814d521e52fcea631
nitish754 <ernitish1993@gmail.com>
2020-07-03 02:02:35 -0700

    DB migration added

--- database/migrations/2020_07_03_082235_create_products_table.php
+++ database/migrations/2020_07_03_082235_create_products_table.php
@@ -15,6 +15,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('product_name');
+            $table->text('product_details');
+            $table->double('price');
+            $table->double('discount');
+            $table->integer('stock');
+
             $table->timestamps();
         });
     }
